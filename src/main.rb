e56336af9c4584e2c0b8449d402b3038f8f1e4d1
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-11-30 13:29:06 +0200

    start implementing to ternary function

--- src/main.rb
+++ src/main.rb
@@ -1,1 +1,9 @@
-print 'Hello, Cantor Distribution!'
+def to_ternary(x); end
+
+# Tests
+require 'test/unit'
+class TestCantor < Test::Unit::TestCase
+  def test_to_ternary
+    assert_equal(10, to_ternary(3))
+  end
+end
