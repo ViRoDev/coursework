104868b13ac832a40f56222f7c6dff66a38b0efb
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-11-30 14:02:17 +0200

    implement zero case

--- src/main.rb
+++ src/main.rb
@@ -1,7 +1,7 @@
 def to_ternary(x)
   res = ''
 
-  if x % 3 == 0
+  if x % 3 == 0 && x != 0
     x %= 3
     res << '1'
     while x % 3 != 0
@@ -18,5 +18,9 @@ require 'test/unit'
 class TestCantor < Test::Unit::TestCase
   def test_base_ternary
     assert_equal(10.to_s, to_ternary(3))
+  end
+
+  def test_zero_ternary
+    assert_equal(0.to_s, to_ternary(0))
   end
 end
