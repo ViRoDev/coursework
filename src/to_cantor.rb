e8bf795bb8154bfe0bed8a2cf3469a705e2471a5
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-06 21:48:22 +0200

    refactor

--- src/to_cantor.rb
+++ src/to_cantor.rb
@@ -14,15 +14,26 @@ def replace_every_digit_after_first_1(string)
 end
 
 class String
+  def fractional_part?
+    !to_f.modulo(1).eql?(0.0)
+  end
+
+  def integer_part_binary
+    to_f.floor.to_i.to_s(2).to_i
+  end
+
+  def fractional_part_binary
+    to_f.modulo(1).to_s.split('.')[1]
+  end
+
   # With precision up to 3 digits after the dot
   def to_float_from_binary
     base = 2
-    return to_i(2).to_f if to_f.modulo(1).eql?(0.0)
+    return to_i(2).to_f unless fractional_part?
 
     # If not, convert the hard way...
-    integer_part = to_f.floor.to_i.to_s(2).to_i
-
-    fractional_part = to_f.modulo(1).to_s.split('.')[1]
+    integer_part = integer_part_binary
+    fractional_part = fractional_part_binary
 
     one_over_base = 1.0 / base
 
