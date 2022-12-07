28bfc3ff53f159a7a734bac144712bed0b41f2b0
Vitaly Romanov <102828415+ViRoDev@users.noreply.github.com>
2022-12-07 02:59:35 +0200

    load actual data to the chart

--- main.rb
+++ main.rb
@@ -1,13 +1,10 @@
 require_relative 'src/to_cantor'
+require_relative 'src/data'
 require 'sinatra'
 require 'chartkick'
 
 get '/' do
-  @title = 'TEST'
-  @data = [
-    %w[Washington 1789-04-29 1797-03-03],
-    %w[Adams 1797-03-03 1801-03-03],
-    %w[Jefferson 1801-03-03 1809-03-03]
-  ]
+  @title = 'Cantor Distribution'
+  @data = get_data
   erb :chart
 end
